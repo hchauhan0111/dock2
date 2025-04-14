906e27f737cb806122c81aef12ee563eff6a9ced
hchauhan0111 <himanshu.chauhan@nagarro.com>
2025-04-14 11:41:14 +0530

    docker sh file fix

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Enter the image tag version (e.g., v1, v1-build-18): " TAG
 
-COMPOSE_FILE="docker-compose.yml"
+COMPOSE_FILE="docker-compose.yaml"
 cp $COMPOSE_FILE "${COMPOSE_FILE}.bak"
 
 sed -i "s|himanshuchauhan1/backend_service:.*|himanshuchauhan1/backend_service:$TAG|g" $COMPOSE_FILE
