03963111567194fe481856051e4410a1e6588aff
hchauhan0111 <himanshu.chauhan@nagarro.com>
2025-04-14 11:27:14 +0530

    Create docker-compose-up.sh

---
+++ docker-compose-up.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "Enter the image tag version (e.g., v1, v1-build-18): " TAG
+
+COMPOSE_FILE="docker-compose.yml"
+cp $COMPOSE_FILE "${COMPOSE_FILE}.bak"
+
+sed -i "s|himanshuchauhan1/backend_service:.*|himanshuchauhan1/backend_service:$TAG|g" $COMPOSE_FILE
+sed -i "s|himanshuchauhan1/my-react-app:.*|himanshuchauhan1/my-react-app:$TAG|g" $COMPOSE_FILE
+
+docker-compose up -d
